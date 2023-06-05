7b20de3162efb7a1761facd2a3065c67f7028568
Pedro Samuel <pedrosviki1265@gmail.com>
2023-06-05 13:23:21 -0400

    configurando o componente botão na calculadora

--- src/components/botao/index.jsx
+++ src/components/botao/index.jsx
@@ -1,7 +1,13 @@
 import './style.css';
 
 function botao(props){
-  return <button className='botao'>{props.value}</button>
+  return <button className={`botao 
+                                  ${props.operation ? 'operation' : ''}
+                                  ${props.double ? 'double' : ''}
+                                  ${props.triple ? 'triple' : ''}`}      
+                          onClick={(e) => props.click && props.click(e.target.innerHTML)}> 
+      {props.value}
+    </button>
 }
 
 export default botao;
--- src/page/calculadora.jsx
+++ src/page/calculadora.jsx
@@ -1,31 +1,54 @@
+import { Component } from "react";
 import Botao from "../components/botao/index.jsx";
 import Display from "../components/display/index.jsx";
 import "./calculadora.css";
 
-function Calculadora() {
-  return <>
-    <h1>CALCULADORA</h1>
-    <div className="calculadora">
-      <Display value="100"/>
-      <Botao value="AC"/>
-      <Botao value="/"/>
-      <Botao value="9"/>
-      <Botao value="8"/>
-      <Botao value="7"/>
-      <Botao value="*"/>
-      <Botao value="6"/>
-      <Botao value="5"/>
-      <Botao value="4"/>
-      <Botao value="-"/>
-      <Botao value="3"/>
-      <Botao value="2"/>
-      <Botao value="1"/>
-      <Botao value="+"/>
-      <Botao value="0"/>
-      <Botao value="="/>
-      <Botao value="."/>
-    </div>
-  </>;
+class Calculadora extends Component{
+
+  constructor(props) {
+    super(props)
+    this.Limpar = this.Limpar.bind(this)
+    this.setOperation = this.setOperation.bind(this)
+    this.addDig = this.addDig.bind(this)
+  }
+
+  Limpar(){
+    console.log('limpar');
+  }
+
+  setOperation(operation){
+    console.log(operation);
+  }
+
+  addDig(n) {
+    console.log(n);
+  }
+
+  render() {
+    return <>
+      <h1>CALCULADORA</h1>
+      <div className="calculadora">
+        <Display value="100"/>
+        <Botao value="AC" click={this.Limpar} triple/>
+        <Botao value="/" click={this.setOperation} operation/>
+        <Botao value="7" click={this.addDig}/>
+        <Botao value="8" click={this.addDig}/>
+        <Botao value="9" click={this.addDig}/>
+        <Botao value="*" click={this.setOperation} operation/>
+        <Botao value="4" click={this.addDig}/>
+        <Botao value="5" click={this.addDig}/>
+        <Botao value="6" click={this.addDig}/>
+        <Botao value="-" click={this.setOperation} operation/>
+        <Botao value="1" click={this.addDig}/>
+        <Botao value="2" click={this.addDig}/>
+        <Botao value="3" click={this.addDig}/>
+        <Botao value="+" click={this.setOperation} operation/>
+        <Botao value="0" click={this.addDig} double/>
+        <Botao value="." click={this.setOperation}/>
+        <Botao value="=" click={this.addDig} operation/>
+      </div>
+    </>;
+  }
 }
 
 export default Calculadora;
