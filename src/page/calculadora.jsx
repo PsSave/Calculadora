085f71aa121386bae0c6ac886d6b610157a8e07d
Pedro Samuel <pedrosviki1265@gmail.com>
2023-06-06 10:24:29 -0400

    implementando a lógica da calculadora

--- src/page/calculadora.jsx
+++ src/page/calculadora.jsx
@@ -3,7 +3,17 @@ import Botao from "../components/botao/index.jsx";
 import Display from "../components/display/index.jsx";
 import "./calculadora.css";
 
+const initialState ={ 
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0
+}
+
 class Calculadora extends Component{
+
+  state = {...initialState}
 
   constructor(props) {
     super(props)
@@ -13,23 +23,65 @@ class Calculadora extends Component{
   }
 
   Limpar(){
-    console.log('limpar');
+    this.setState({...initialState})
   }
 
   setOperation(operation){
-    console.log(operation);
+    if(this.state.current == 0) {
+      this.setState({current: 1, operation, clearDisplay: true});
+    } else {
+      const equals = operation == '='
+      const opCorrente = this.state.operation
+
+      const values = [...this.state.values]
+      try{
+        values[0] = eval(`${values[0]} ${opCorrente} ${values[1]}`)
+        if (isNaN(values[0]) || !isFinite(values[0])) {
+          this.clearMemory()
+        return
+        }
+      } catch{
+        values[0] = this.state.values[0]
+      }
+
+      values[1] = 0
+
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values
+      })
+    }
   }
 
   addDig(n) {
-    console.log(n);
+    if(n === '.' && this.state.displayValue.includes('.')) {
+      return 
+    }
+
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+    const valorCorrente = clearDisplay ? '' : this.state.displayValue
+    const displayValue = valorCorrente + n
+    this.setState({displayValue, clearDisplay: false});
+
+    if(n !== '.') {
+      const i = this.state.current
+      const newValue =  parseFloat(displayValue) 
+      const values = [...this.state.values]
+      values[i] = newValue
+      this.setState({ values })
+    }
+
   }
 
   render() {
     return <>
       <h1>CALCULADORA</h1>
       <div className="calculadora">
-        <Display value="100"/>
-        <Botao value="AC" click={this.Limpar} triple/>
+        <Display value={this.state.displayValue}/>
+        <Botao value="AC" click={this.Limpar} triple />
         <Botao value="/" click={this.setOperation} operation/>
         <Botao value="7" click={this.addDig}/>
         <Botao value="8" click={this.addDig}/>
@@ -44,8 +96,8 @@ class Calculadora extends Component{
         <Botao value="3" click={this.addDig}/>
         <Botao value="+" click={this.setOperation} operation/>
         <Botao value="0" click={this.addDig} double/>
-        <Botao value="." click={this.setOperation}/>
-        <Botao value="=" click={this.addDig} operation/>
+        <Botao value="." click={this.addDig}/>
+        <Botao value="=" click={this.setOperation} operation/>
       </div>
     </>;
   }
